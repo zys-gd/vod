702330a35ef927e20de66851568c33e5a9b8580d
yurii.zakharenko-at-024590668976 <mD/gaYppzFdMznwWJfpOqL7dJMBOlgAOIqzHqAbWCA4=>
2019-01-10 17:03:25 +0200

    * fake piwik tracker

---
+++ src/App/Domain/Constants/ConstFakeTrackerActions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain\Constants;
+
+class ConstFakeTrackerActions
+{
+    const TRACK_SUBSCRIBE = 'trackSubscribe';
+    const TRACK_RESUBSCRIBE = 'trackResubscribe';
+    const TRACK_RENEW = 'trackRenew';
+    const TRACK_UNSUBSCRIBE = 'trackUnsubscribe';
+    const TRACK_DOWNLOAD = 'trackDownload';
+}
