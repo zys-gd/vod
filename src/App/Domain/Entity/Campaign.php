2da954a7e93b6a00bae9800b1c51b3dfa09b081b
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-01-21 17:09:06 +0200

    Affiliate module

--- src/App/Domain/Entity/Campaign.php
+++ src/App/Domain/Entity/Campaign.php
@@ -179,7 +179,7 @@ class Campaign implements CampaignInterface
     /**
      * @return string
      */
-    public function getCampaignToken() {
+    public function getCampaignToken(): string {
         return $this->campaignToken;
     }
 
@@ -411,7 +411,7 @@ class Campaign implements CampaignInterface
         return $this;
     }
 
-    public function getSub()
+    public function getSub(): string
     {
         return $this->sub;
     }
