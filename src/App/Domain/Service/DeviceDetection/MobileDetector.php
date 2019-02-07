8be2c50b67abae4bad978bebe152f848e1ba9443
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-02-07 17:15:48 +0200

    Wros functionality

---
+++ src/App/Domain/Service/DeviceDetection/MobileDetector.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitriy
+ * Date: 19.04.18
+ * Time: 14:07
+ */
+
+namespace App\Domain\Service\DeviceDetection;
+
+
+use Mobile_Detect;
+
+class MobileDetector
+{
+    public function isAndroidOS()
+    {
+        $mobileDetect = new Mobile_Detect();
+
+        return $mobileDetect->isAndroid();
+
+    }
+}
