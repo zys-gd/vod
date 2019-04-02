8ad81e5f33c0e64b3d372b85a6fb010c87623dc7
Yurii Z <mD/gaYppzFdMznwWJfpOqL7dJMBOlgAOIqzHqAbWCA4=>
2019-04-02 15:10:14 +0300

    * magic numbers

--- src/IdentificationBundle/Carriers/MobilinkPK/MobilinkPKIdentificationCallbackHandler.php
+++ src/IdentificationBundle/Carriers/MobilinkPK/MobilinkPKIdentificationCallbackHandler.php
@@ -9,6 +9,7 @@
 namespace IdentificationBundle\Carriers\MobilinkPK;
 
 
+use App\Domain\Constants\ConstBillingCarrierId;
 use IdentificationBundle\Entity\User;
 use IdentificationBundle\Callback\Handler\HasCommonFlow;
 use IdentificationBundle\Callback\Handler\IdentCallbackHandlerInterface;
@@ -20,7 +21,7 @@ class MobilinkPKIdentificationCallbackHandler implements
 {
     public function canHandle(int $carrierId): bool
     {
-        return $carrierId === 338;
+        return $carrierId === ConstBillingCarrierId::MOBILINK_PAKISTAN;
     }
 
     public function afterSuccess(User $billableUser, ProcessResult $processResponse): void
