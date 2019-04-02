ae32ce2b8ac3e27c2b44bdf970a6ea9926bfe70c
daniel.kleba-at-987157480843 <daniel.kleba@playwing.com>
2019-04-02 15:46:31 +0300

    Add symfony command for cleaning old entries in audit logs table, also add command for cleaning entries with empty blame_id(trash)

---
+++ src/App/Command/AuditClearEmptyBlameLogsCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Command;
+
+use DataDog\AuditBundle\Entity\AuditLog;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use \Doctrine\DBAL\ConnectionException;
+use \Throwable;
+
+class AuditClearEmptyBlameLogsCommand extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'app:audit:clear-empty-blame-logs';
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /**
+     * AuditClearEmptyBlameLogsCommand constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Command that clear log entries with an empty blame_id');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     * @throws ConnectionException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $this->entityManager->getConnection()->beginTransaction();
+        try {
+            $queryBuilder = $this->entityManager->createQueryBuilder();
+
+            $queryBuilder->delete(AuditLog::class, 'a')
+                ->where('a.blame is null')
+                ->getQuery()
+                ->execute();
+
+            $this->entityManager->getConnection()->commit();
+
+            $io->success('Successfully deleted log entries with an empty blame_id');
+
+        } catch (Throwable $e) {
+            $this->entityManager->getConnection()->rollBack();
+
+            $io->error($e->getMessage());
+        }
+    }
+}
---
+++ src/App/Command/AuditClearOldLogEntriesCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use DataDog\AuditBundle\Entity\AuditLog;
+use \Doctrine\DBAL\ConnectionException;
+use \DateTime;
+use \Throwable;
+
+class AuditClearOldLogEntriesCommand extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'app:audit:clear-old-log-entries';
+
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /**
+     * AdminClearOldLogEntriesCommand constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Command will clear log entries older than a month');
+    }
+
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     * @throws ConnectionException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+
+        $this->entityManager->getConnection()->beginTransaction();
+        try {
+            $queryBuilder = $this->entityManager->createQueryBuilder();
+
+            $queryBuilder->delete(AuditLog::class, 'a')
+                ->where("a.loggedAt < :month_ago")
+                ->setParameters(['month_ago' => new DateTime('- 1 month')])
+                ->getQuery()
+                ->execute();
+
+            $this->entityManager->getConnection()->commit();
+
+            $io->success('Successfully deleted log entries older than a month');
+
+        } catch (Throwable $e) {
+            $this->entityManager->getConnection()->rollBack();
+
+            $io->error($e->getMessage());
+        }
+    }
+}
