0208f03f138e183e0b1dea37bd33887d52e08257
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-04-08 18:21:34 +0300

    Missing annotations hotfix

--- src/App/Controller/GamesController.php
+++ src/App/Controller/GamesController.php
@@ -14,6 +14,7 @@ use App\Domain\Service\Games\ExcludedGamesProvider;
 use App\Domain\Service\Games\GameImagesSerializer;
 use App\Domain\Service\Games\GameSerializer;
 use IdentificationBundle\Identification\DTO\ISPData;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use SubscriptionBundle\Entity\Subscription;
 use SubscriptionBundle\Service\SubscriptionExtractor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,6 +22,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
 
 class GamesController extends AbstractController implements AppControllerInterface
 {
