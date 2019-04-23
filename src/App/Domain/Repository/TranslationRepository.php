9365b4270e5b9d30422e38d6a3598000728e8614
Yurii Z <mD/gaYppzFdMznwWJfpOqL7dJMBOlgAOIqzHqAbWCA4=>
2019-04-23 16:24:07 +0300

    * fix faq

--- src/App/Domain/Repository/TranslationRepository.php
+++ src/App/Domain/Repository/TranslationRepository.php
@@ -2,16 +2,6 @@
 
 namespace App\Domain\Repository;
 
-use App\Domain\Entity\Carrier;
-use App\Domain\Entity\Translation;
-use App\Domain\Service\FaqProviderService;
-use App\Exception\WrongTranslationRecordType;
-use AppBundle\Entity\Language;
-use Doctrine\ORM\NativeQuery;
-use Doctrine\ORM\Query\ResultSetMapping;
-use Doctrine\ORM\Query\ResultSetMappingBuilder;
-use IdentificationBundle\Entity\CarrierInterface;
-
 class TranslationRepository extends \Doctrine\ORM\EntityRepository
 {
 }
