ab88eab84decbb4b52a62e8910be9cbb98321960
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-05-10 16:57:34 +0300

    EWVS-104: Loggers

--- src/SubscriptionBundle/Service/SubscriptionLimiter/Limiter/CarrierCapChecker.php
+++ src/SubscriptionBundle/Service/SubscriptionLimiter/Limiter/CarrierCapChecker.php
@@ -10,6 +10,7 @@ namespace SubscriptionBundle\Service\SubscriptionLimiter\Limiter;
 
 
 use IdentificationBundle\Entity\CarrierInterface;
+use Psr\Log\LoggerInterface;
 
 class CarrierCapChecker
 {
@@ -21,27 +22,40 @@ class CarrierCapChecker
      * @var StorageKeyGenerator
      */
     private $storageKeyGenerator;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * CarrierCapChecker constructor.
      * @param LimiterStorage      $limiterDataStorage
      * @param StorageKeyGenerator $storageKeyGenerator
+     * @param LoggerInterface     $logger
      */
-    public function __construct(LimiterStorage $limiterDataStorage, StorageKeyGenerator $storageKeyGenerator)
+    public function __construct(LimiterStorage $limiterDataStorage, StorageKeyGenerator $storageKeyGenerator, LoggerInterface $logger)
     {
         $this->limiterDataStorage  = $limiterDataStorage;
         $this->storageKeyGenerator = $storageKeyGenerator;
+        $this->logger              = $logger;
     }
 
     public function isCapReachedForCarrier(CarrierInterface $carrier): bool
     {
         $key = $this->storageKeyGenerator->generateKey($carrier);
 
-        $pending    = $this->limiterDataStorage->getPendingSubscriptionAmount($key);
-        $finished   = $this->limiterDataStorage->getFinishedSubscriptionAmount($key);
-        $totalCount = $pending + $finished;
+        $pending      = $this->limiterDataStorage->getPendingSubscriptionAmount($key);
+        $finished     = $this->limiterDataStorage->getFinishedSubscriptionAmount($key);
+        $totalCount   = $pending + $finished;
+        $carrierLimit = $carrier->getNumberOfAllowedSubscriptionsByConstraint();
 
-        return $totalCount > $carrier->getNumberOfAllowedSubscriptionsByConstraint();
+        $this->logger->debug('Carrier cap check', [
+            'pending'      => $pending,
+            'finished'     => $finished,
+            'carrierLimit' => $carrierLimit
+        ]);
+
+        return $totalCount > $carrierLimit;
 
     }
 
@@ -49,10 +63,19 @@ class CarrierCapChecker
     {
         $key = $this->storageKeyGenerator->generateAffiliateConstraintKey($constraintByAffiliate);
 
-        $pending    = $this->limiterDataStorage->getPendingSubscriptionAmount($key);
-        $finished   = $this->limiterDataStorage->getFinishedSubscriptionAmount($key);
-        $totalCount = $pending + $finished;
+        $pending        = $this->limiterDataStorage->getPendingSubscriptionAmount($key);
+        $finished       = $this->limiterDataStorage->getFinishedSubscriptionAmount($key);
+        $totalCount     = $pending + $finished;
+        $affiliateLimit = $constraintByAffiliate->getNumberOfActions();
 
-        return $totalCount > $constraintByAffiliate->getNumberOfActions();
+
+        $this->logger->debug('Affiliate cap check', [
+            'pending'        => $pending,
+            'finished'       => $finished,
+            'affiliateLimit' => $affiliateLimit
+        ]);
+
+
+        return $totalCount > $affiliateLimit;
     }
 }
