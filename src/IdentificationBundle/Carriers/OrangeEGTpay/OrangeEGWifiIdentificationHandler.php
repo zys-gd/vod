782a8b16ec90dbf98a73b600fd6dad24797d59a0
Denis Lukash <denis.lukash@origin-data.com>
2019-05-16 14:49:53 +0300

    EWVS-126 - wifi flow for orange

--- src/IdentificationBundle/Carriers/OrangeEGTpay/OrangeEGWifiIdentificationHandler.php
+++ src/IdentificationBundle/Carriers/OrangeEGTpay/OrangeEGWifiIdentificationHandler.php
@@ -3,29 +3,49 @@
 namespace IdentificationBundle\Carriers\OrangeEGTpay;
 
 use App\Domain\Constants\ConstBillingCarrierId;
+use Doctrine\ORM\EntityManagerInterface;
+use IdentificationBundle\BillingFramework\Process\DTO\PinRequestResult;
+use IdentificationBundle\BillingFramework\Process\DTO\PinVerifyResult;
 use IdentificationBundle\Entity\CarrierInterface;
 use IdentificationBundle\Entity\User;
 use IdentificationBundle\Repository\UserRepository;
+use IdentificationBundle\WifiIdentification\Exception\WifiIdentConfirmException;
+use IdentificationBundle\WifiIdentification\Handler\HasCustomPinVerifyRules;
 use IdentificationBundle\WifiIdentification\Handler\WifiIdentificationHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Class OrangeEGWifiIdentificationHandler
  */
-class OrangeEGWifiIdentificationHandler implements WifiIdentificationHandlerInterface
+class OrangeEGWifiIdentificationHandler implements WifiIdentificationHandlerInterface, HasCustomPinVerifyRules
 {
     /**
      * @var UserRepository
      */
-    private $repository;
+    private $userRepository;
 
     /**
-     * VodafonePKWifiIdentificationHandler constructor
-     *
-     * @param UserRepository $repository
+     * @var EntityManagerInterface
      */
-    public function __construct(UserRepository $repository)
+    private $entityManager;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * OrangeEGWifiIdentificationHandler constructor
+     *
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $entityManager
+     * @param RouterInterface $router
+     */
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager, RouterInterface $router)
     {
-        $this->repository = $repository;
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
     }
 
     /**
@@ -36,10 +56,6 @@ class OrangeEGWifiIdentificationHandler implements WifiIdentificationHandlerInte
     public function canHandle(CarrierInterface $carrier): bool
     {
         return $carrier->getBillingCarrierId() === ConstBillingCarrierId::ORANGE_EGYPT_TPAY;
-    }
-
-    public function getRedirectUrl()
-    {
     }
 
     /**
@@ -57,6 +73,65 @@ class OrangeEGWifiIdentificationHandler implements WifiIdentificationHandlerInte
      */
     public function getExistingUser(string $msisdn): ?User
     {
-        return $this->repository->findOneByMsisdn($msisdn);
+        return $this->userRepository->findOneByMsisdn($msisdn);
+    }
+
+    /**
+     * @param PinRequestResult $pinRequestResult
+     *
+     * @return array
+     */
+    public function getAdditionalPinVerifyParams(PinRequestResult $pinRequestResult): array
+    {
+        $data = $pinRequestResult->getRawData();
+
+        if (empty($data['subscription_contract_id']) || empty($data['transactionId'])) {
+            throw new WifiIdentConfirmException("Can't process pin verification. Missing required parameters");
+        }
+
+        return ['client_user' => $data['subscription_contract_id'], 'transactionId' => $data['transactionId']];
+    }
+
+    /**
+     * @param PinVerifyResult $pinVerifyResult
+     * @param string $phoneNumber
+     *
+     * @return string
+     */
+    public function getMsisdnFromResult(PinVerifyResult $pinVerifyResult, string $phoneNumber): string
+    {
+        return $phoneNumber;
+    }
+
+    /**
+     * @param PinVerifyResult $parameters
+     * @param User $user
+     */
+    public function afterSuccessfulPinVerify(PinVerifyResult $parameters, User $user): void
+    {
+        $data = $parameters->getRawData();
+
+        if (!empty($data['subscription_contract_id'])) {
+            $user->setProviderId($data['subscription_contract_id']);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getRedirectUrl()
+    {
+        return $this->router->generate('subscription.consent_page_subscribe');
+    }
+
+    /**
+     * @param \Exception $exception
+     */
+    public function afterFailedPinVerify(\Exception $exception): void
+    {
+
     }
 }
