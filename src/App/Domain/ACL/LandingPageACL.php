7222f6dd8ccbcbd1d9e553f06056c9f3a7b4b674
yu.zakharenko-at-987157480843 <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-05-16 13:44:28 +0300

    EWVS-104 * add logger to Lp ACL

--- src/App/Domain/ACL/LandingPageACL.php
+++ src/App/Domain/ACL/LandingPageACL.php
@@ -11,6 +11,7 @@ use App\Domain\ACL\Exception\SubscriptionCapReachedOnCarrier;
 use App\Domain\ACL\Exception\VisitCapReached;
 use App\Domain\Entity\Campaign;
 use App\Domain\Entity\Carrier;
+use Psr\Log\LoggerInterface;
 use SubscriptionBundle\Entity\Affiliate\ConstraintByAffiliate;
 use SubscriptionBundle\Service\CAPTool\Limiter\SubscriptionCapChecker;
 
@@ -33,6 +34,10 @@ class LandingPageACL
      * @var SubscriptionCapChecker
      */
     private $carrierCapChecker;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * LandingPageAccessResolver constructor
@@ -40,34 +45,41 @@ class LandingPageACL
      * @param VisitConstraintByAffiliate $visitConstraintByAffiliate
      * @param VisitAccessorByCampaign    $visitAccessorByCampaign
      * @param SubscriptionCapChecker     $subscriptionCapChecker
+     * @param LoggerInterface            $logger
      */
     public function __construct(
         VisitConstraintByAffiliate $visitConstraintByAffiliate,
         VisitAccessorByCampaign $visitAccessorByCampaign,
-        SubscriptionCapChecker $subscriptionCapChecker
+        SubscriptionCapChecker $subscriptionCapChecker,
+        LoggerInterface $logger
     )
     {
         $this->visitConstraintByAffiliate = $visitConstraintByAffiliate;
         $this->visitAccessorByCampaign    = $visitAccessorByCampaign;
         $this->carrierCapChecker          = $subscriptionCapChecker;
+        $this->logger = $logger;
     }
 
     /**
      * @param Campaign $campaign
      * @param Carrier  $carrier
+     *
      * @return void
      */
     public function ensureCanAccess(Campaign $campaign, Carrier $carrier): void
     {
         if ($campaign->getIsPause()) {
+            $this->logger->debug('Campaign on pause');
             throw new CampaignPausedException();
         }
 
         if (!$this->visitAccessorByCampaign->canVisit($campaign, $carrier)) {
+            $this->logger->debug('visitAccessorByCampaign say: cant visit');
             throw new CampaignAccessException($campaign);
         }
 
         if ($this->carrierCapChecker->isCapReachedForCarrier($carrier)) {
+            $this->logger->debug('isCapReachedForCarrier say: isCapReachedForCarrier = true');
             throw new SubscriptionCapReachedOnCarrier($carrier);
         }
 
@@ -81,12 +93,18 @@ class LandingPageACL
 
             if ($constraint->getCapType() == ConstraintByAffiliate::CAP_TYPE_SUBSCRIBE) {
                 if ($this->carrierCapChecker->isCapReachedForAffiliate($constraint)) {
+                    $this->logger->debug('Constraint by aff say: isCapReachedForAffiliate = true', [
+                        'constraint' => $constraint
+                    ]);
                     throw new SubscriptionCapReachedOnAffiliate($constraint, $carrier);
                 }
             }
 
             if ($constraint->getCapType() == ConstraintByAffiliate::CAP_TYPE_VISIT) {
                 if (!$this->visitConstraintByAffiliate->canVisit($carrier, $constraint)) {
+                    $this->logger->debug('Constraint by aff say: CanVisit = false', [
+                        'constraint' => $constraint
+                    ]);
                     throw new VisitCapReached($constraint);
                 }
             }
