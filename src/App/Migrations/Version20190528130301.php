cd7e6f90d4dfcfdb65b8837f4fdef99b10df5df1
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-05-31 13:03:46 +0300

    EWVS-147: Translations

--- src/App/Migrations/Version20190528130301.php
+++ src/App/Migrations/Version20190528130301.php
@@ -13,8 +13,8 @@ final class Version20190528130301 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql("
-INSERT IGNORE INTO translations (`uuid`,`key`,`translation`,`language_id`,`carrier_id`) VALUES ('7b3115d1-04db-40a3-838c-2e01d5b34872','messages.error.subscription_restricted','Subscription Restricted placeholder AR',(SELECT `uuid` FROM `languages` WHERE `uuid` = '5179ee29-ebd4-11e8-95c4-02bb250f0f22'),(SELECT `uuid` FROM `carriers` WHERE `uuid` = ''));
-INSERT IGNORE INTO translations (`uuid`,`key`,`translation`,`language_id`,`carrier_id`) VALUES ('42a13111-2caa-4075-9226-13c4b9d2c412','messages.error.subscription_restricted','Subscription Restricted placeholder EN',(SELECT `uuid` FROM `languages` WHERE `uuid` = '5179f17c-ebd4-11e8-95c4-02bb250f0f22'),(SELECT `uuid` FROM `carriers` WHERE `uuid` = ''));
+INSERT IGNORE INTO translations (`uuid`,`key`,`translation`,`language_id`,`carrier_id`) VALUES ('7b3115d1-04db-40a3-838c-2e01d5b34872','messages.error.subscription_restricted','Dear customer, sorry, but you are already subscribed or have been subscribed to one of our services, in this regard, the possibility of a subscription for you is limited and impossible',(SELECT `uuid` FROM `languages` WHERE `uuid` = '5179ee29-ebd4-11e8-95c4-02bb250f0f22'),(SELECT `uuid` FROM `carriers` WHERE `uuid` = ''));
+INSERT IGNORE INTO translations (`uuid`,`key`,`translation`,`language_id`,`carrier_id`) VALUES ('42a13111-2caa-4075-9226-13c4b9d2c412','messages.error.subscription_restricted','Dear customer, sorry, but you are already subscribed or have been subscribed to one of our services, in this regard, the possibility of a subscription for you is limited and impossible',(SELECT `uuid` FROM `languages` WHERE `uuid` = '5179f17c-ebd4-11e8-95c4-02bb250f0f22'),(SELECT `uuid` FROM `carriers` WHERE `uuid` = ''));
 ");
 
     }
