e0154aaddea55c23bcee275e547c738a081857f2
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-06-04 11:53:27 +0300

    EWVS-165: * fix migration

--- src/App/Migrations/Version20190603091749.php
+++ src/App/Migrations/Version20190603091749.php
@@ -18,7 +18,6 @@ final class Version20190603091749 extends AbstractMigration
         $this->addSql('CREATE TABLE affiliate_carrier (affiliate_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', carrier_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_1ADE3A2E9F12C49A (affiliate_id), INDEX IDX_1ADE3A2E21DFC797 (carrier_id), PRIMARY KEY(affiliate_id, carrier_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE affiliate_carrier ADD CONSTRAINT FK_1ADE3A2E9F12C49A FOREIGN KEY (affiliate_id) REFERENCES affiliates (uuid)');
         $this->addSql('ALTER TABLE affiliate_carrier ADD CONSTRAINT FK_1ADE3A2E21DFC797 FOREIGN KEY (carrier_id) REFERENCES carriers (uuid)');
-        $this->addSql('ALTER TABLE campaigns DROP free_trial_subscription');
     }
 
     public function down(Schema $schema) : void
@@ -27,6 +26,5 @@ final class Version20190603091749 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE affiliate_carrier');
-        $this->addSql('ALTER TABLE campaigns ADD free_trial_subscription TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 }
