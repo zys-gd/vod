57ab31b8c10f4df145d508702bb5a2e4bffad56d
Denis Lukash <denis.lukash@origin-data.com>
2019-06-12 13:42:08 +0300

    EWVS-126 - review identification bundle

--- src/IdentificationBundle/Twig/IdentificationStatusExtension.php
+++ src/IdentificationBundle/Twig/IdentificationStatusExtension.php
@@ -4,6 +4,8 @@ namespace IdentificationBundle\Twig;
 
 use App\Domain\Entity\Carrier;
 use App\Domain\Repository\CarrierRepository;
+use IdentificationBundle\Identification\Handler\ConsentPageFlow\HasConsentPageFlow;
+use IdentificationBundle\Identification\Handler\IdentificationHandlerProvider;
 use IdentificationBundle\Identification\Service\IdentificationDataStorage;
 use IdentificationBundle\Identification\Service\IdentificationFlowDataExtractor;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -27,22 +29,29 @@ class IdentificationStatusExtension extends AbstractExtension
      * @var CarrierRepository
      */
     private $carrierRepository;
+    /**
+     * @var IdentificationHandlerProvider
+     */
+    private $identificationHandlerProvider;
 
     /**
      * IdentificationStatusExtension constructor.
      *
      * @param IdentificationDataStorage $dataStorage
-     * @param SessionInterface          $session
-     * @param CarrierRepository         $carrierRepository
+     * @param SessionInterface $session
+     * @param CarrierRepository $carrierRepository
+     * @param IdentificationHandlerProvider $identificationHandlerProvider
      */
     public function __construct(
         IdentificationDataStorage $dataStorage,
         SessionInterface $session,
-        CarrierRepository $carrierRepository
+        CarrierRepository $carrierRepository,
+        IdentificationHandlerProvider $identificationHandlerProvider
     ) {
         $this->dataStorage = $dataStorage;
         $this->session = $session;
         $this->carrierRepository = $carrierRepository;
+        $this->identificationHandlerProvider = $identificationHandlerProvider;
     }
 
     /**
@@ -61,8 +70,18 @@ class IdentificationStatusExtension extends AbstractExtension
             }),
 
             new TwigFunction('isConsentFlow', function () {
-                $token = $this->dataStorage->readValue('consentFlow[token]');
-                return (bool)$token;
+                $ispDetectionData = IdentificationFlowDataExtractor::extractIspDetectionData($this->session);
+                $carrierId =  empty($ispDetectionData['carrier_id']) ? null : (int) $ispDetectionData['carrier_id'];
+                $isConsent = null;
+
+                if ($carrierId) {
+                    $identHandler = $this->identificationHandlerProvider->get($this->carrierRepository->findOneByBillingId($carrierId));
+                    $isConsent =  $identHandler instanceof HasConsentPageFlow;
+                } else {
+                    $isConsent = (bool) $this->dataStorage->readValue('consentFlow[token]');
+                }
+
+                return $isConsent;
             }),
 
             new TwigFunction('isWifiFlow', function () {
