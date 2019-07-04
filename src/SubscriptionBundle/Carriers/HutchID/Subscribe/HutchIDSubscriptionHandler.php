a4bc6c805f012803ee702675c749e851c43b99ea
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-07-04 13:09:14 +0300

    EWVS-190 * add hutch subscribers

---
+++ src/SubscriptionBundle/Carriers/HutchID/Subscribe/HutchIDSubscriptionHandler.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace SubscriptionBundle\Carriers\HutchID\Subscribe;
+
+use App\Domain\Constants\ConstBillingCarrierId;
+use ExtrasBundle\Utils\LocalExtractor;
+use IdentificationBundle\Entity\CarrierInterface;
+use IdentificationBundle\Entity\User;
+use IdentificationBundle\Identification\Service\IdentificationDataStorage;
+use SubscriptionBundle\BillingFramework\Process\API\DTO\ProcessResult;
+use SubscriptionBundle\BillingFramework\Process\Exception\SubscribingProcessException;
+use SubscriptionBundle\Entity\Subscription;
+use SubscriptionBundle\Service\Action\Subscribe\Handler\ConsentPageFlow\HasConsentPageFlow;
+use SubscriptionBundle\Service\Action\Subscribe\Handler\SubscriptionHandlerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+
+
+class HutchIDSubscriptionHandler implements SubscriptionHandlerInterface, HasConsentPageFlow
+{
+    /**
+     * @var IdentificationDataStorage
+     */
+    private $identificationDataStorage;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * HutchIDSubscriptionHandler constructor.
+     *
+     * @param IdentificationDataStorage $identificationDataStorage
+     * @param RouterInterface           $router
+     */
+    public function __construct(
+        IdentificationDataStorage $identificationDataStorage,
+        RouterInterface $router
+    ) {
+        $this->identificationDataStorage = $identificationDataStorage;
+        $this->router = $router;
+    }
+
+    /**
+     * @param CarrierInterface $carrier
+     *
+     * @return bool
+     */
+    public function canHandle(CarrierInterface $carrier): bool
+    {
+        return $carrier->getBillingCarrierId() === ConstBillingCarrierId::HUTCH_INDONESIA;
+    }
+
+    /**
+     * @param Request $request
+     * @param User $user
+     *
+     * @return array
+     */
+    public function getAdditionalSubscribeParams(Request $request, User $user): array
+    {
+
+        return [];
+    }
+
+    /**
+     * @param SubscribingProcessException $exception
+     *
+     * @return Response
+     */
+    public function getSubscriptionErrorResponse(SubscribingProcessException $exception): Response
+    {
+        $billingData = $exception->getBillingData();
+        $failReason = $billingData->provider_fields->fail_reason;
+
+        switch ($failReason) {
+            case SubscribingProcessException::FAIL_REASON_NOT_ENOUGH_CREDIT:
+                $redirectUrl = $this->router->generate('index', ['err_handle' => 'not_enough_credit']);
+                break;
+            case SubscribingProcessException::FAIL_REASON_BLACKLISTED:
+                $redirectUrl = $this->router->generate('blacklisted_user');
+                break;
+            default:
+                $redirectUrl = $this->router->generate('whoops');
+                break;
+        }
+
+        return new RedirectResponse($redirectUrl);
+    }
+
+    /**
+     * @param Subscription $subscription
+     * @param ProcessResult $result
+     */
+    public function afterProcess(Subscription $subscription, ProcessResult $result): void
+    {
+
+    }
+}
---
+++ src/SubscriptionBundle/Carriers/HutchID/Unsubscribe/HutchIDUnsubscribeHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SubscriptionBundle\Carriers\HutchID\Unsubscribe;
+
+use App\Domain\Constants\ConstBillingCarrierId;
+use ExtrasBundle\Utils\LocalExtractor;
+use IdentificationBundle\Entity\CarrierInterface;
+use SubscriptionBundle\BillingFramework\Process\API\DTO\ProcessResult;
+use SubscriptionBundle\Entity\Subscription;
+use SubscriptionBundle\Service\Action\Unsubscribe\Handler\UnsubscriptionHandlerInterface;
+
+class HutchIDUnsubscribeHandler implements UnsubscriptionHandlerInterface
+{
+    /**
+     * @param CarrierInterface $carrier
+     *
+     * @return bool
+     */
+    public function canHandle(CarrierInterface $carrier): bool
+    {
+        return $carrier->getBillingCarrierId() === ConstBillingCarrierId::HUTCH_INDONESIA;
+    }
+
+    /**
+     * @param ProcessResult $processResult
+     *
+     * @return bool
+     */
+    public function isPiwikNeedToBeTracked(ProcessResult $processResult): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAdditionalUnsubscribeParameters(): array
+    {
+        return [];
+    }
+
+    /**
+     * @param Subscription $subscription
+     */
+    public function applyPostUnsubscribeChanges(Subscription $subscription)
+    {
+
+    }
+}
