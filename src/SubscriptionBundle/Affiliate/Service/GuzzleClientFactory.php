f3ef58c72f812be1170b3a8c7c03a371972ed509
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-07-22 18:37:45 +0300

    Hotfix: Timeout issue

--- src/SubscriptionBundle/Affiliate/Service/GuzzleClientFactory.php
+++ src/SubscriptionBundle/Affiliate/Service/GuzzleClientFactory.php
@@ -15,6 +15,6 @@ class GuzzleClientFactory
 {
     public function getClient(): Client
     {
-        return new Client(['connect_timeout' => 1, 'read_timeout' => 1,'timeout' => 1]);
+        return new Client(['connect_timeout' => 5, 'read_timeout' => 5,'timeout' => 5]);
     }
 }
