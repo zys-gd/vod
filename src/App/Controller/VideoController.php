509ebf290ccf1db385b2e06f4f52cc838a090df1
Denis Lukash <denis.lukash@origin-data.com>
2019-07-26 13:17:23 +0300

    EWVS-245 - display reusable video template in player

--- src/App/Controller/VideoController.php
+++ src/App/Controller/VideoController.php
@@ -89,56 +89,8 @@ class VideoController extends AbstractController implements AppControllerInterfa
             $serializedData[] = $this->videoSerializer->serializeShort($video);
         }
 
-        $html = $this->renderView('@App/Components/category_player_related_videos.html.twig', [
+        $html = $this->renderView('@App/Components/related_videos.html.twig', [
             'videos'   => $serializedData
-        ]);
-
-        return new JsonResponse([
-            'html' => $html,
-            'isLast' => $videos->isLast()
-        ]);
-    }
-
-    /**
-     * @Route("/videos/load-more", methods={"GET"}, name="load_more_related_videos")
-     *
-     * @param ISPData $data
-     * @param IdentificationData $identificationData
-     * @param Request $request
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     */
-    public function loadMoreRelatedVideoAction(ISPData $data, IdentificationData $identificationData, Request $request)
-    {
-
-        if (!$categoryUuid = $request->get('categoryUuid')) {
-            throw new BadRequestHttpException('Missing `categoryUuid` parameter');
-        }
-
-        /** @var MainCategory $category */
-        if (!$category = $this->mainCategoryRepository->findOneBy(['uuid' => $categoryUuid])) {
-            throw new NotFoundHttpException('Category not found');
-        }
-
-        $offset = $request->get('offset', 0);
-
-        /** @var BatchOfNotExpiredVideos $videos */
-        $videos = $this->videoRepository->findNotExpiredBySubcategories(
-            $category->getSubcategories()->toArray(),
-            $offset,
-            20
-        );
-
-        $serializedData = [];
-
-        foreach ($videos->getVideos() as $video) {
-            $serializedData[] = $this->videoSerializer->serializeShort($video);
-        }
-
-        $html = $this->renderView('@App/Components/player_related_videos.html.twig', [
-            'videos' => $serializedData,
-            'category' => $category
         ]);
 
         return new JsonResponse([
