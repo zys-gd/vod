57e3f59237eb0f937294ecf78523a343e0fa944a
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-08-06 15:27:55 +0300

    EWVS-190 * fixes

--- src/App/Migrations/Version20190806120254.php
+++ src/App/Migrations/Version20190806120254.php
@@ -8,12 +8,12 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190805133003 extends AbstractMigration
+final class Version20190806120254 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("INSERT INTO translations (uuid, language_id, carrier_id, `key`, translation) VALUES ('5125f53c-3206-4665-afb3-eee9e5b15661', '5179f466-ebd4-11e8-95c4-02bb250f0f22', '038275ac-dae1-446a-8eac-17a26fb69ea2', 'messages.subscription_popup.confirmation_text', 'Hi %phone%, you will be subscribed to service 100% sport with rate %currency% %price%/%period%');");
+        $this->addSql("INSERT INTO translations (uuid, language_id, carrier_id, `key`, translation) VALUES ('5125f53c-3206-4665-afb3-eee9e5b15661', '5179f466-ebd4-11e8-95c4-02bb250f0f22', '038275ac-dae1-446a-8eac-17a26fb69ea2', 'messages.subscription_popup.confirmation_text', 'Hai %phone%, Anda akan berlangganan layanan 100% sport dengan tarif %currency% %price%/%period');");
 
         $this->addSql("INSERT INTO translations (uuid, language_id, carrier_id, `key`, translation) VALUES ('b00fe180-17e2-4663-a2ed-2278b707ea27', '5179f466-ebd4-11e8-95c4-02bb250f0f22', '038275ac-dae1-446a-8eac-17a26fb69ea2', 'messages.subscription_popup.confirmation_title', '');");
     }
--- src/App/Twig/UserInformationExtension.php
+++ src/App/Twig/UserInformationExtension.php
@@ -5,35 +5,56 @@ namespace App\Twig;
 
 
 use IdentificationBundle\Identification\DTO\IdentificationData;
+use IdentificationBundle\Identification\Handler\HasHeaderEnrichment;
+use IdentificationBundle\Identification\Handler\IdentificationHandlerProvider;
 use IdentificationBundle\Identification\Service\Session\IdentificationFlowDataExtractor;
+use IdentificationBundle\Repository\CarrierRepositoryInterface;
 use SubscriptionBundle\Service\UserExtractor;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class UserInformationExtension extends AbstractExtension
 {
-
     /**
      * @var UserExtractor
      */
     private $userExtractor;
     /**
-     * @var SessionInterface
+     * @var IdentificationHandlerProvider
      */
-    private $session;
+    private $handlerProvider;
+    /**
+     * @var CarrierRepositoryInterface
+     */
+    private $carrierRepository;
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
 
     /**
      * UserInformationExtension constructor.
      *
-     * @param SessionInterface $session
-     * @param UserExtractor    $userExtractor
+     * @param RequestStack                  $requestStack
+     * @param UserExtractor                 $userExtractor
+     * @param IdentificationHandlerProvider $handlerProvider
+     * @param CarrierRepositoryInterface    $carrierRepository
      */
-    public function __construct(SessionInterface $session, UserExtractor $userExtractor)
+    public function __construct(RequestStack $requestStack,
+        UserExtractor $userExtractor,
+        IdentificationHandlerProvider $handlerProvider,
+        CarrierRepositoryInterface $carrierRepository)
     {
-        $this->userExtractor = $userExtractor;
-        $this->session       = $session;
+        $this->userExtractor     = $userExtractor;
+        $this->handlerProvider   = $handlerProvider;
+        $this->carrierRepository = $carrierRepository;
+        $this->requestStack      = $requestStack;
     }
 
     public function getFunctions()
@@ -49,12 +70,21 @@ class UserInformationExtension extends AbstractExtension
      */
     public function getUserIdentifier(): ?string
     {
-        if (!$identificationToken = IdentificationFlowDataExtractor::extractIdentificationToken($this->session)) {
-            throw new BadRequestHttpException('Identification data is not found');
+        $identificationToken = IdentificationFlowDataExtractor::extractIdentificationToken($this->requestStack->getCurrentRequest()->getSession());
+        $billingCarrierId    = IdentificationFlowDataExtractor::extractBillingCarrierId($this->requestStack->getCurrentRequest()->getSession());
+
+        $carrier = $this->carrierRepository->findOneByBillingId($billingCarrierId);
+        $handler = $this->handlerProvider->get($carrier);
+        if ($handler instanceof HasHeaderEnrichment) {
+            return $handler->getMsisdn($this->requestStack->getCurrentRequest());
         }
 
-        $identificationData = new IdentificationData($identificationToken);
-        $user = $this->userExtractor->getUserByIdentificationData($identificationData);
-        return $user->getIdentifier();
+        try {
+            $identificationData = new IdentificationData($identificationToken);
+            $user               = $this->userExtractor->getUserByIdentificationData($identificationData);
+            return $user->getIdentifier();
+        } catch (\Throwable $e) {
+            return null;
+        }
     }
 }
