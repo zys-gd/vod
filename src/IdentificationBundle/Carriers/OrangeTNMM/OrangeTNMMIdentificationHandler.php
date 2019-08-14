d225e9019df56a7dd3d4f6ca969fa8aabe92f781
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-08-14 17:24:41 +0300

    EWVS-266 + add passthrough ident for orange TN MM 3g

---
+++ src/IdentificationBundle/Carriers/OrangeTNMM/OrangeTNMMIdentificationHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace IdentificationBundle\Carriers\HutchID;
+
+use App\Domain\Constants\ConstBillingCarrierId;
+use IdentificationBundle\Entity\CarrierInterface;
+use IdentificationBundle\Identification\Handler\HasHeaderEnrichment;
+use IdentificationBundle\Identification\Handler\IdentificationHandlerInterface;
+use IdentificationBundle\Identification\Handler\PassthroughFlow\HasPassthroughFlow;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class OrangeTNMMIdentificationHandler implements
+    IdentificationHandlerInterface,
+    HasPassthroughFlow
+{
+    /**
+     * @param CarrierInterface $carrier
+     *
+     * @return bool
+     */
+    public function canHandle(CarrierInterface $carrier): bool
+    {
+        return $carrier->getBillingCarrierId() === ConstBillingCarrierId::ORANGE_TUNISIA_MM;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function getAdditionalIdentificationParams(Request $request): array
+    {
+        return [];
+    }
+}
