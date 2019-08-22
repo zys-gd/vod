da040d09787135f8f07e31a5a278d3df9c5fa94b
Daniel Kleba <klebadanial@gmail.com>
2019-08-22 15:34:44 +0300

    fix loadCarrierFixtures

--- src/DataFixtures/LoadCarriersData.php
+++ src/DataFixtures/LoadCarriersData.php
@@ -55,7 +55,7 @@ class LoadCarriersData extends AbstractFixture implements ContainerAwareInterfac
             $flushdate                               = $row['flushDate'];
 
 
-            $defaultLanguageId = $row['default_language']['uuid'];
+            $defaultLanguageId = $row['defaultLanguage']['uuid'];
 
             $carrier = new Carrier($uuid);
 
