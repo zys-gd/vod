974fca67e16b2643f078b279fd01dcc9d3d23bbf
Daniel Kleba <klebadanial@gmail.com>
2019-08-22 13:27:16 +0300

    EWVS-225: Fix notes

--- src/App/Domain/Repository/CountryRepository.php
+++ src/App/Domain/Repository/CountryRepository.php
@@ -24,24 +24,4 @@ class CountryRepository extends EntityRepository
 
         return $qb->getQuery()->getResult(AbstractQuery::HYDRATE_OBJECT);
     }
-
-    /**
-     * @param int $billingCarrierId
-     * @return mixed
-     * @throws \Doctrine\ORM\NoResultException
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function findCountryByBillingCarrierId(int $billingCarrierId)
-    {
-        $qb = $this->createQueryBuilder('co');
-
-        $query = $qb
-            ->leftJoin(Carrier::class, 'ca','WITH', 'co.countryCode = ca.countryCode')
-            ->where('ca.billingCarrierId = :billingCarrierId')
-            ->setParameter('billingCarrierId', $billingCarrierId)
-            ->select('co')
-            ->getQuery();
-
-        return $query->getSingleResult();
-    }
 }
