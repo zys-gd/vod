032c28a2a50c63df25f75b972cbc832b75b6193a
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-08-28 12:32:54 +0300

    Some ident process fixes

--- src/IdentificationBundle/WifiIdentification/Service/IdentFinisher.php
+++ src/IdentificationBundle/WifiIdentification/Service/IdentFinisher.php
@@ -3,8 +3,10 @@
 namespace IdentificationBundle\WifiIdentification\Service;
 
 use CommonDataBundle\Entity\Interfaces\CarrierInterface;
+use DeviceDetectionBundle\Service\Device;
 use Doctrine\ORM\EntityManagerInterface;
 use IdentificationBundle\Entity\User;
+use IdentificationBundle\Identification\DTO\DeviceData;
 use IdentificationBundle\Identification\Service\Session\IdentificationDataStorage;
 use IdentificationBundle\Identification\Service\TokenGenerator;
 
@@ -42,7 +44,8 @@ class IdentFinisher
         \IdentificationBundle\User\Service\UserFactory $userFactory,
         IdentificationDataStorage $identificationDataStorage,
         EntityManagerInterface $entityManager
-    ) {
+    )
+    {
         $this->tokenGenerator            = $tokenGenerator;
         $this->userFactory               = $userFactory;
         $this->identificationDataStorage = $identificationDataStorage;
@@ -50,18 +53,19 @@ class IdentFinisher
     }
 
     /**
-     * @param string $msisdn
+     * @param string           $msisdn
      * @param CarrierInterface $carrier
-     * @param string $ip
+     * @param string           $ip
      *
+     * @param DeviceData       $deviceData
      * @return User
      *
      * @throws \Exception
      */
-    public function finish(string $msisdn, CarrierInterface $carrier, string $ip): User
+    public function finish(string $msisdn, CarrierInterface $carrier, string $ip, DeviceData $deviceData): User
     {
         $token = $this->tokenGenerator->generateToken();
-        $user  = $this->userFactory->create($msisdn, $carrier, $ip, $token);
+        $user  = $this->userFactory->create($msisdn, $carrier, $ip, $token, null, $deviceData);
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
@@ -71,11 +75,11 @@ class IdentFinisher
     }
 
     /**
-     * @param User $user
+     * @param User   $user
      * @param string $msisdn
      * @param string $ip
      */
-    public function finishForExistingUser(User $user, string $msisdn, string $ip): void
+    public function finishForExistingUser(User $user, string $msisdn, string $ip, DeviceData $deviceData): void
     {
         $token = $this->tokenGenerator->generateToken();
 
@@ -85,6 +89,13 @@ class IdentFinisher
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+
+        $user->setDeviceManufacturer($deviceData->getDeviceManufacturer());
+        $user->setDeviceModel($deviceData->getDeviceModel());
+        $user->setConnectionType($deviceData->getConnectionType());
+        $user->setIdentificationUrl($deviceData->getIdentificationUrl());
+
 
         $this->identificationDataStorage->setIdentificationToken($token);
     }
