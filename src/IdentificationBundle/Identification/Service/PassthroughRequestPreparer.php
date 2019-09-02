c3ffec6287b86f2e21b143211a891f3e66aa8874
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-09-02 16:49:47 +0300

    EWVS-292: Affiliate data sending with every ident process starting

--- src/IdentificationBundle/Identification/Service/PassthroughRequestPreparer.php
+++ src/IdentificationBundle/Identification/Service/PassthroughRequestPreparer.php
@@ -24,6 +24,10 @@ class PassthroughRequestPreparer
      * @var TokenGenerator
      */
     private $generator;
+    /**
+     * @var AffiliateDataSerializer
+     */
+    private $affiliateDataSerializer;
 
     /**
      * PassthroughRequestPreparer constructor.
@@ -31,27 +35,32 @@ class PassthroughRequestPreparer
      * @param RouterInterface           $router
      * @param RequestParametersProvider $requestParametersProvider
      * @param TokenGenerator            $generator
+     * @param AffiliateDataSerializer   $affiliateDataSerializer
      */
     public function __construct(
         RouterInterface $router,
         RequestParametersProvider $requestParametersProvider,
-        TokenGenerator $generator
+        TokenGenerator $generator,
+        AffiliateDataSerializer $affiliateDataSerializer
     )
     {
         $this->router                    = $router;
         $this->requestParametersProvider = $requestParametersProvider;
         $this->generator                 = $generator;
+        $this->affiliateDataSerializer   = $affiliateDataSerializer;
     }
 
     public function getProcessRequestParameters(Request $request): ProcessRequestParameters
     {
+        $affiliateParams     = $this->affiliateDataSerializer->serialize($request->getSession());
         $identificationToken = IdentificationFlowDataExtractor::extractIdentificationToken($request->getSession())
-                                ?? $this->generator->generateToken();
+            ?? $this->generator->generateToken();
         $billingCarrierId    = IdentificationFlowDataExtractor::extractBillingCarrierId($request->getSession());
         $successUrl          = $this->router->generate('subscription.subscribe_back', [], RouterInterface::ABSOLUTE_URL);
         $waitPageUrl         = $this
             ->router
             ->generate('wait_for_callback', ['successUrl' => $successUrl], RouterInterface::ABSOLUTE_URL);
+
 
         $parameters = $this->requestParametersProvider->prepareRequestParameters(
             $identificationToken,
@@ -59,7 +68,7 @@ class PassthroughRequestPreparer
             $request->getClientIp(),
             $waitPageUrl,
             $request->headers->all(),
-            []
+            $affiliateParams
         );
 
         return $parameters;
