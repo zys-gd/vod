4a42d18d3c0cbf2a61ec9fc59107276c36c592c5
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-09-19 13:16:51 +0300

    EWVS-305: Remove debug dummy

--- src/SubscriptionBundle/Subscription/Subscribe/ProcessStarter/CommonStarter.php
+++ src/SubscriptionBundle/Subscription/Subscribe/ProcessStarter/CommonStarter.php
@@ -80,7 +80,7 @@ class CommonStarter implements SubscribeProcessStarterInterface
             return $this->subscribePerformer->doSubscribe($subscription, $additionalData);
         }
 
-        if (false || $isFreeTrialSubscriptionFromCampaign) {
+        if ($pack->isFirstSubscriptionPeriodIsFree() || $isFreeTrialSubscriptionFromCampaign) {
             return $this->sendNotificationPerformer->doSentNotification($subscription);
         }
 
