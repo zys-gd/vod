423d79c8d539e09a1d80e82366b1d1a2266ef430
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-09-27 16:26:44 +0300

    EWVS-305: Fix for text

---
+++ src/App/Migrations/Version20190927132542.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190927132542 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->up("
+UPDATE translations SET `translation` = 'Dear customer, we are sorry but you are already subscribed to some other subscription service. If you want to subscribe to 100sport.tv, please contact us.' WHERE `translations`.`uuid` = '3df66c6d-f478-4f58-9964-af76c5e18480'        
+        ");
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->down("
+        UPDATE translations SET `translation` = 'Dear customer, we are sorry but you are already subscribed to some other subscription service. If you want to subscribe to Playwing, please contact us.' WHERE `translations`.`uuid` = '3df66c6d-f478-4f58-9964-af76c5e18480'");
+
+    }
+}
