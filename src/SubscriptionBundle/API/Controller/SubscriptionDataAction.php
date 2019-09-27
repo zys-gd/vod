3965c8bc041ff5690dd68d147f2eaf5f85aa2082
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-09-27 12:12:50 +0300

    Hotfix: Additional logging

--- src/SubscriptionBundle/API/Controller/SubscriptionDataAction.php
+++ src/SubscriptionBundle/API/Controller/SubscriptionDataAction.php
@@ -10,6 +10,7 @@ namespace SubscriptionBundle\API\Controller;
 
 
 use ExtrasBundle\Controller\Traits\ResponseTrait;
+use Psr\Log\LoggerInterface;
 use SubscriptionBundle\API\Exception\NotFoundException;
 use SubscriptionBundle\API\Service\LegacyBillingFormatter;
 use SubscriptionBundle\API\Service\SubscriptionInfoProvider;
@@ -29,6 +30,10 @@ class SubscriptionDataAction
      * @var SubscriptionInfoProvider
      */
     private $provider;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
 
     /**
@@ -36,10 +41,11 @@ class SubscriptionDataAction
      * @param LegacyBillingFormatter   $legacyBillingFormatter
      * @param SubscriptionInfoProvider $provider
      */
-    public function __construct(LegacyBillingFormatter $legacyBillingFormatter, SubscriptionInfoProvider $provider)
+    public function __construct(LegacyBillingFormatter $legacyBillingFormatter, SubscriptionInfoProvider $provider, LoggerInterface $logger)
     {
         $this->legacyBillingFormatter = $legacyBillingFormatter;
         $this->provider               = $provider;
+        $this->logger                 = $logger;
     }
 
     public function __invoke(Request $request)
@@ -60,6 +66,9 @@ class SubscriptionDataAction
         } catch (NotFoundException $exception) {
             return new JsonResponse(['error' => $exception->getMessage()], Response::HTTP_NOT_FOUND);
         } catch (\Exception $exception) {
+
+            $this->logger->error($exception->getMessage(), ['trace' => $exception->getTrace()]);
+
             return new JsonResponse(['error' => 'Something has gone wrong'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
