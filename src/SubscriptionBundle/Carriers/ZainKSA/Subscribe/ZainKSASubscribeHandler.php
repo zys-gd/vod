3278ece57ac73a62d4aa80f27a99f3824f7bb9b1
Denis Lukash <denis.lukash@origin-data.com>
2019-10-09 17:28:09 +0300

    EWVS-353 - custom tracking rules for Zain KSA

--- src/SubscriptionBundle/Carriers/ZainKSA/Subscribe/ZainKSASubscribeHandler.php
+++ src/SubscriptionBundle/Carriers/ZainKSA/Subscribe/ZainKSASubscribeHandler.php
@@ -2,11 +2,15 @@
 
 namespace SubscriptionBundle\Carriers\ZainKSA\Subscribe;
 
+use App\Domain\Repository\CarrierRepository;
 use IdentificationBundle\BillingFramework\ID;
 use IdentificationBundle\Entity\User;
 use SubscriptionBundle\BillingFramework\Process\API\DTO\ProcessResult;
+use SubscriptionBundle\Entity\Affiliate\CampaignInterface;
 use SubscriptionBundle\Entity\Subscription;
+use SubscriptionBundle\Subscription\Subscribe\Common\ZeroCreditSubscriptionChecking;
 use SubscriptionBundle\Subscription\Subscribe\Handler\HasCommonFlow;
+use SubscriptionBundle\Subscription\Subscribe\Handler\HasCustomAffiliateTrackingRules;
 use SubscriptionBundle\Subscription\Subscribe\Handler\SubscriptionHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use CommonDataBundle\Entity\Interfaces\CarrierInterface;
@@ -14,8 +18,32 @@ use CommonDataBundle\Entity\Interfaces\CarrierInterface;
 /**
  * Class ZainKSASubscribeHandler
  */
-class ZainKSASubscribeHandler implements SubscriptionHandlerInterface, HasCommonFlow
+class ZainKSASubscribeHandler implements SubscriptionHandlerInterface, HasCommonFlow, HasCustomAffiliateTrackingRules
 {
+    /**
+     * @var CarrierRepository
+     */
+    private $carrierRepository;
+
+    /**
+     * @var ZeroCreditSubscriptionChecking
+     */
+    private $zeroCreditSubscriptionChecking;
+
+    /**
+     * ZainKSASubscribeHandler constructor.
+     *
+     * @param CarrierRepository              $carrierRepository
+     * @param ZeroCreditSubscriptionChecking $zeroCreditSubscriptionChecking
+     */
+    public function __construct(
+        CarrierRepository $carrierRepository,
+        ZeroCreditSubscriptionChecking $zeroCreditSubscriptionChecking
+    ) {
+        $this->carrierRepository = $carrierRepository;
+        $this->zeroCreditSubscriptionChecking = $zeroCreditSubscriptionChecking;
+    }
+
     /**
      * @param CarrierInterface $carrier
      *
@@ -24,6 +52,38 @@ class ZainKSASubscribeHandler implements SubscriptionHandlerInterface, HasCommon
     public function canHandle(CarrierInterface $carrier): bool
     {
         return $carrier->getBillingCarrierId() === ID::ZAIN_SAUDI_ARABIA;
+    }
+
+    /**
+     * @param ProcessResult     $result
+     * @param CampaignInterface $campaign
+     *
+     * @return bool
+     */
+    public function isAffiliateTrackedForSub(ProcessResult $result, CampaignInterface $campaign): bool
+    {
+        $carrier = $this->carrierRepository->findOneByBillingId(ID::ZAIN_SAUDI_ARABIA);
+
+        $isSuccess = $result->isFailedOrSuccessful() && $result->isFinal();
+        $isZeroCreditsSub = $this
+            ->zeroCreditSubscriptionChecking
+            ->isZeroCreditAvailable(ID::ZAIN_SAUDI_ARABIA, $campaign);
+
+        if ($isZeroCreditsSub) {
+            return $isSuccess && $carrier->getTrackAffiliateOnZeroCreditSub();
+        }
+
+        return $isSuccess;
+    }
+
+    /**
+     * @param ProcessResult $result
+     *
+     * @return bool
+     */
+    public function isAffiliateTrackedForResub(ProcessResult $result): bool
+    {
+        return false;
     }
 
     /**
