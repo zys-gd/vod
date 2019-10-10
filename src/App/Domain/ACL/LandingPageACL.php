99b971a9b02aaf13661a160da10ea3c93a7f91a4
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-10-10 15:04:39 +0300

    * logging isOneClickFlow

--- src/App/Domain/ACL/LandingPageACL.php
+++ src/App/Domain/ACL/LandingPageACL.php
@@ -167,7 +167,7 @@ class LandingPageACL
             $isSupportRequestedFlow = $this->oneClickFlowChecker->check($carrier->getBillingCarrierId(), OneClickFlowParameters::LP_OFF);
             $this->logger->debug('Inside isLandingDisabled()', [
                 '$isSupportRequestedFlow' => $isSupportRequestedFlow,
-                'isOneClickFlow' => $carrier->isOneClickFlow(),
+                'isOneClickFlow'          => $carrier->isOneClickFlow(),
             ]);
 
             if ($carrier->isOneClickFlow() && $isSupportRequestedFlow) {
@@ -184,12 +184,12 @@ class LandingPageACL
                     $isLPOffByCampaign = $campaign->isOneClickFlow() && $isCampaignScheduleExistAndTriggered;
 
                     $this->logger->debug('Inside isLandingDisabled() campaign check', [
-                        '$isLPOffByCampaign' => $isLPOffByCampaign,
-                        '$isLPOffByAffiliate' => $isLPOffByAffiliate,
+                        '$isLPOffByCampaign'               => $isLPOffByCampaign,
+                        '$isLPOffByAffiliate'              => $isLPOffByAffiliate,
                         '$affiliate->hasCarrier($carrier)' => $affiliate->hasCarrier($carrier),
                         'empty($affiliate->getCarriers())' => empty($affiliate->getCarriers())
                     ]);
-                    return  $isLPOffByAffiliate && $isLPOffByCampaign;
+                    return $isLPOffByAffiliate && $isLPOffByCampaign;
                 }
 
                 return true;
