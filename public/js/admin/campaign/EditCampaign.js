008399cc67fa0b3bea053b39810859769485693a
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-10-17 16:41:30 +0300

    EWVS-297: * reverse schedule intervals

--- public/js/admin/campaign/EditCampaign.js
+++ public/js/admin/campaign/EditCampaign.js
@@ -1,7 +1,7 @@
 let containerForFields,
     standardNameForDiv;
 
-function hideOrShowOptionForOneClickIfCheckedIsOneClickFlow(){
+function hideOrShowOptionForOneClickIfCheckedIsOneClickFlow() {
     let isOneClickFlow = $('input[id*="_isOneClickFlow"]')[0];
     let whenActiveOneClickFlowCalendar = $('div[id*="_schedule"]').first();
     let isOneClickFlowOnOutOfOffice = $('div[id*="isOneClickFlowOnOutOfOffice"]').first();
@@ -16,7 +16,7 @@ function hideOrShowOptionForOneClickIfCheckedIsOneClickFlow(){
     }
 }
 
-function hideOrShowTableIfCheckedPermanently(){
+function hideOrShowTableIfCheckedPermanently() {
     let isOneClickFlow = $('input[id*="_isOneClickFlow"]')[0];
     let whenActiveOneClickFlowCalendar = $('div[id*="_schedule"]').first();
     let isOneClickFlowOnOutOfOffice = $('div[id*="isOneClickFlowOnOutOfOffice"]').first();
@@ -33,33 +33,33 @@ function hideOrShowTableIfCheckedPermanently(){
     }
 }
 
-function DataForOnOutOfOfficeOrArabicGeo(arrabicGeo){
+function DataForOnOutOfOfficeOrArabicGeo(arrabicGeo) {
 
     let isOneClickFlowOnOutOfOffice = $('input[id*="isOneClickFlowOnOutOfOffice"]').first();
     let isOneClickFlowOnOutOfOfficeArabicGeo = $('input[id*="isOneClickFlowOnOutOfOfficeArabicGeo"]').first();
     let dataForTable = null;
 
-    if(arrabicGeo){
-        if(isOneClickFlowOnOutOfOfficeArabicGeo.is(':checked')){
-            if(isOneClickFlowOnOutOfOffice.is(':checked')){
+    if (arrabicGeo) {
+        if (isOneClickFlowOnOutOfOfficeArabicGeo.is(':checked')) {
+            if (isOneClickFlowOnOutOfOffice.is(':checked')) {
                 $(isOneClickFlowOnOutOfOffice).siblings().click();
             }
-            dataForTable = '{"day":0,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":1,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":2,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":3,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":4,"periods":[{"start":"00:00","end":"00:00"}]},{"day":5,"periods":[{"start":"00:00","end":"00:00"}]},{"day":6,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]}';
+            dataForTable = '{"day":0,"periods":[{"start":"08:00","end":"21:00"}]},{"day":1,"periods":[{"start":"08:00","end":"21:00"}]},{"day":2,"periods":[{"start":"08:00","end":"21:00"}]},{"day":3,"periods":[{"start":"08:00","end":"21:00"}]},{"day":6,"periods":[{"start":"08:00","end":"21:00"}]}';
         }
-    } else if(!arrabicGeo){
-        if(isOneClickFlowOnOutOfOffice.is(':checked')){
-            if(isOneClickFlowOnOutOfOfficeArabicGeo.is(':checked')){
+    } else if (!arrabicGeo) {
+        if (isOneClickFlowOnOutOfOffice.is(':checked')) {
+            if (isOneClickFlowOnOutOfOfficeArabicGeo.is(':checked')) {
                 $(isOneClickFlowOnOutOfOfficeArabicGeo).siblings().click();
             }
-            dataForTable = '{"day":0,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":1,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":2,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":3,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":4,"periods":[{"start":"00:00","end":"08:00"},{"start":"21:00","end":"00:00"}]},{"day":5,"periods":[{"start":"00:00","end":"00:00"}]},{"day":6,"periods":[{"start":"00:00","end":"00:00"}]}';
+            dataForTable = '{"day":0,"periods":[{"start":"08:00","end":"21:00"}]},{"day":1,"periods":[{"start":"08:00","end":"21:00"}]},{"day":2,"periods":[{"start":"08:00","end":"21:00"}]},{"day":3,"periods":[{"start":"08:00","end":"21:00"}]},{"day":4,"periods":[{"start":"08:00","end":"21:00"}]}';
         }
     }
 
-    if(dataForTable === null){
+    if (dataForTable === null) {
         $("#schedule").jqs().jqs('reset');
-    } else{
+    } else {
         $("#schedule").jqs().jqs('reset');
-        $("#schedule").jqs().jqs('import', JSON.parse( '[' + dataForTable + ']'));
+        $("#schedule").jqs().jqs('import', JSON.parse('[' + dataForTable + ']'));
     }
 }
 
@@ -67,7 +67,7 @@ $(document).ready(function () {
     let input = $('input[id*="_schedule"]')[0];
     containerForFields = $('input[id*="_schedule"]').prev('div');
     standardNameForDiv = '_schedule';
-    $(containerForFields).append("<div class='form-group' id='"+ standardNameForDiv +"'><div id='schedule'></div><div id='scheduler'></div></div>");
+    $(containerForFields).append("<div class='form-group' id='" + standardNameForDiv + "'><div id='schedule'></div><div id='scheduler'></div></div>");
 
     hideOrShowOptionForOneClickIfCheckedIsOneClickFlow();
     hideOrShowTableIfCheckedPermanently();
@@ -83,10 +83,10 @@ $(document).ready(function () {
         DataForOnOutOfOfficeOrArabicGeo(arabicGeo);
     });
 
-    $("#schedule").jqs().jqs('import', JSON.parse( '[' + $(input).val() + ']'));
+    $("#schedule").jqs().jqs('import', JSON.parse('[' + $(input).val() + ']'));
 
     $(document).on('click', 'button[name="btn_update_and_list"], button[name="btn_update_and_edit"], button[name="btn_create_and_edit"] ,button[name="btn_create_and_list"], button[name="btn_create_and_create"]', function (e) {
-        let newExportString =  $('#schedule').jqs('export');
+        let newExportString = $('#schedule').jqs('export');
         newExportString = newExportString.slice(1, -1);
         $('input[id*="_schedule"]').val(newExportString);
     });
