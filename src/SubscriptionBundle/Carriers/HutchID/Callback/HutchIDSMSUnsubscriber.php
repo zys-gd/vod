37e9375336d1026fdc0309dd2eab6ce4c7f29f09
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-10-21 17:32:21 +0300

    * update tracking unsub via SMS on hutchID

--- src/SubscriptionBundle/Carriers/HutchID/Callback/HutchIDSMSUnsubscriber.php
+++ src/SubscriptionBundle/Carriers/HutchID/Callback/HutchIDSMSUnsubscriber.php
@@ -8,6 +8,8 @@ use SubscriptionBundle\BillingFramework\Process\API\DTO\ProcessResult;
 use SubscriptionBundle\Entity\Subscription;
 use SubscriptionBundle\Service\EntitySaveHelper;
 use SubscriptionBundle\Subscription\Callback\Common\Handler\UnsubscriptionCallbackHandler;
+use SubscriptionBundle\Subscription\Unsubscribe\Unsubscriber;
+use Playwing\CrossSubscriptionAPIBundle\Connector\ApiConnector;
 
 class HutchIDSMSUnsubscriber
 {
@@ -19,19 +21,45 @@ class HutchIDSMSUnsubscriber
      * @var EntitySaveHelper
      */
     private $entitySaveHelper;
+    /**
+     * @var Unsubscriber
+     */
+    private $unsubscriber;
+    /**
+     * @var ApiConnector
+     */
+    private $crossSubscriptionApi;
 
+    /**
+     * HutchIDSMSUnsubscriber constructor.
+     *
+     * @param UnsubscriptionCallbackHandler $unsubscriptionCallbackHandler
+     * @param EntitySaveHelper              $entitySaveHelper
+     * @param Unsubscriber                  $unsubscriber
+     * @param ApiConnector                  $apiConnector
+     */
     public function __construct(
         UnsubscriptionCallbackHandler $unsubscriptionCallbackHandler,
-        EntitySaveHelper $entitySaveHelper
+        EntitySaveHelper $entitySaveHelper,
+        Unsubscriber $unsubscriber,
+        ApiConnector $apiConnector
     )
     {
         $this->unsubscriptionCallbackHandler = $unsubscriptionCallbackHandler;
         $this->entitySaveHelper              = $entitySaveHelper;
+        $this->unsubscriber                  = $unsubscriber;
+        $this->crossSubscriptionApi          = $apiConnector;
     }
 
     public function unsubscribe(Subscription $subscription, ProcessResult $response)
     {
         $this->unsubscriptionCallbackHandler->doProcess($subscription, $response);
         $this->entitySaveHelper->persistAndSave($subscription);
+
+        if ($response->isSuccessful()) {
+            $user = $subscription->getUser();
+            $this->unsubscriber->trackEventsForUnsubscribe($subscription, $response);
+            $this->crossSubscriptionApi->deregisterSubscription($user->getIdentifier(), $user->getBillingCarrierId());
+        }
     }
 }
