62107037da7312bca21270991651b1b75787c975
Denis Lukash <denis.lukash@origin-data.com>
2019-10-31 13:28:22 +0200

    EWVS-311 - wifi handler

--- src/IdentificationBundle/Carriers/TMobilePolandDimoco/TMobilePolandDimocoWifiIdentificationHandler.php
+++ src/IdentificationBundle/Carriers/TMobilePolandDimoco/TMobilePolandDimocoWifiIdentificationHandler.php
@@ -4,15 +4,20 @@ namespace IdentificationBundle\Carriers\TMobilePolandDimoco;
 
 use CommonDataBundle\Entity\Interfaces\CarrierInterface;
 use IdentificationBundle\BillingFramework\ID;
+use IdentificationBundle\BillingFramework\Process\DTO\PinRequestResult;
+use IdentificationBundle\BillingFramework\Process\DTO\PinVerifyResult;
 use IdentificationBundle\Entity\User;
+use IdentificationBundle\Identification\Service\RouteProvider;
 use IdentificationBundle\Repository\UserRepository;
 use IdentificationBundle\WifiIdentification\DTO\PhoneValidationOptions;
+use IdentificationBundle\WifiIdentification\Exception\WifiIdentConfirmException;
+use IdentificationBundle\WifiIdentification\Handler\HasCustomPinVerifyRules;
 use IdentificationBundle\WifiIdentification\Handler\WifiIdentificationHandlerInterface;
 
 /**
  * Class TMobilePolandDimocoWifiIdentificationHandler
  */
-class TMobilePolandDimocoWifiIdentificationHandler implements WifiIdentificationHandlerInterface
+class TMobilePolandDimocoWifiIdentificationHandler implements WifiIdentificationHandlerInterface, HasCustomPinVerifyRules
 {
     /**
      * @var UserRepository
@@ -20,13 +25,20 @@ class TMobilePolandDimocoWifiIdentificationHandler implements WifiIdentification
     private $repository;
 
     /**
+     * @var RouteProvider
+     */
+    private $routeProvider;
+
+    /**
      * TMobilePolandDimocoWifiIdentificationHandler constructor
      *
      * @param UserRepository $repository
+     * @param RouteProvider  $routeProvider
      */
-    public function __construct(UserRepository $repository)
+    public function __construct(UserRepository $repository, RouteProvider $routeProvider)
     {
         $this->repository = $repository;
+        $this->routeProvider = $routeProvider;
     }
 
     /**
@@ -70,8 +82,55 @@ class TMobilePolandDimocoWifiIdentificationHandler implements WifiIdentification
         return false;
     }
 
+    /**
+     * @return string
+     */
     public function getRedirectUrl()
     {
-        // TODO: Implement getRedirectUrl() method.
+        return $this->routeProvider->getLinkToHomepage();
+    }
+
+    /**
+     * @param PinRequestResult $pinRequestResult
+     * @param bool             $isZeroCreditSubAvailable
+     *
+     * @return array
+     */
+    public function getAdditionalPinVerifyParams(PinRequestResult $pinRequestResult, bool $isZeroCreditSubAvailable): array
+    {
+        $data = $pinRequestResult->getRawData();
+
+        if (empty($data['transactionId'])) {
+            throw new WifiIdentConfirmException("Can't process pin verification. Missing required parameters");
+        }
+
+        return ['transactionId' => $data['transactionId']];
+    }
+
+    /**
+     * @param PinVerifyResult $pinVerifyResult
+     * @param string          $phoneNumber
+     *
+     * @return string
+     */
+    public function getMsisdnFromResult(PinVerifyResult $pinVerifyResult, string $phoneNumber): string
+    {
+        return str_replace('+', '', $phoneNumber);
+    }
+
+    /**
+     * @param PinVerifyResult $parameters
+     */
+    public function afterSuccessfulPinVerify(PinVerifyResult $parameters): void
+    {
+        // TODO: Implement afterSuccessfulPinVerify() method.
+    }
+
+    /**
+     * @param \Exception $exception
+     */
+    public function afterFailedPinVerify(\Exception $exception): void
+    {
+        // TODO: Implement afterFailedPinVerify() method.
     }
 }
