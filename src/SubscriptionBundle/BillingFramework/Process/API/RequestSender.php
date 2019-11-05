b4cce55949303b0f0bdd09c010cafd8f953af0fd
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-11-05 17:10:42 +0200

    EWVS-358: Debug fix

--- src/SubscriptionBundle/BillingFramework/Process/API/RequestSender.php
+++ src/SubscriptionBundle/BillingFramework/Process/API/RequestSender.php
@@ -76,13 +76,10 @@ class RequestSender
 
         try {
 
-//            $preparedParams    = $this->extractor->extractParameters($processParameters);
-//            $response          = $this->apiClient->sendPostProcessRequest($preparedParams, $type);
+            $preparedParams    = $this->extractor->extractParameters($processParameters);
+            $response          = $this->apiClient->sendPostProcessRequest($preparedParams, $type);
 
 
-            $response = (object)json_decode('
-            {"environment":{"duration":0.94746,"reverse_key":null,"status":{"id":44,"code":"SUBSCRIBE_OK","group":"success","http_code":"200","success":true},"time":1572825681.1746,"duration_crawling":0.91523814201355},"data":{"id":"29020044","client":"vod-store","client_id":"1bfce6de-090f-457d-9962-0e10e7d087f6","client_user":"923013324837","client_fields":{"listener_wait":"http:\/\/100sport.tv\/v2\/callback\/listen","redirect_url":"http:\/\/100sport.tv","user_ip":"119.160.119.95","user_headers":"host: 100sport.tv\r\naccept: text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/webp,*\/*;q=0.8\r\naccept-encoding: gzip,deflate\r\naccept-language: en-US\r\ncache-control: no-cache\r\npragma: no-cache\r\nreferer: http:\/\/100sport.tv\/identification\/pixel\/show-page?pixelUrl=http%3A\/\/www.dot-jo.biz\/appgw\/DOT.gif%3FtransactionId%3D29019994%26partnerId%3D100sport-cdd4a384&carrier=338&processId=29019994&signature=55e5db4a303a21093429d9dbfc3354b3\r\nuser-agent: Mozilla\/5.0 (Linux; Android 4.4.2; Hol-U19 Build\/HUAWEIHol-U19) AppleWebKit\/537.36 (KHTML, like Gecko) Version\/4.0 Chrome\/30.0.0.0 Mobile Safari\/537.36\r\nx-requested-with: com.app2game.romantic.photo.frames\r\nx-wap-profile: http:\/\/218.249.47.94\/Xianghe\/MTK_Phone_KK_UAprofile.xml\r\nx-forwarded-for: 119.160.119.95\r\nx-forwarded-port: 80\r\nx-forwarded-proto: http\r\nconnection: keep-alive\r\nx-php-ob-level: 1\r\n"},"type":"subscribe","subtype":"final","status":"successful","carrier":"338","provider":"111","charge_full":false,"charge_value":"11.9500","charge_paid":"0","charge_currency":"PKR","charge_product":"1bfce6de-090f-457d-9962-0e10e7d087f6","charge_tier":"2","charge_strategy":"11","insertdate":"2019-11-04 00:01:20.242100","listeners":[{"id":"547348814","link":"http:\/\/100sport.tv\/v2\/callback\/listen","client":"vod-store","process":"29020044","config":{"fields_changed":["status"],"subtypes":["redirect","pixel","direct","final","provider"],"statuses":["successful","failed"]},"bool_undeletable":false,"insertdate":"2019-11-04 00:01:20.247400"}]}}
-            ');
 
             $processedResponse = $this->responseMapper->map($type, $response);
             $this->logger->debug('Received response from billing', [
