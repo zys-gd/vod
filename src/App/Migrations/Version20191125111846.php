a66f1610a5f063463076a887229dc56c043584fa
Denis Lukash <denis.lukash@origin-data.com>
2019-11-25 13:22:08 +0200

    EWVS-381 - add test users

--- src/App/Migrations/Version20191125111846.php
+++ src/App/Migrations/Version20191125111846.php
@@ -12,8 +12,8 @@ final class Version20191125111846 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->addSql("INSERT INTO test_user (uuid, carrier_id, user_identifier) VALUES ('d1dj8f7d-ebd4-12e8-91c4-02bb259d9d8d', '5142f9ec-ebd4-11e8-95c4-02bb250f0f22', '77055247101')");
-        $this->addSql("INSERT INTO test_user (uuid, carrier_id, user_identifier) VALUES ('d1dj8f7d-ebd4-12e8-91c4-02bb2597a6a5', '5142f9ec-ebd4-11e8-95c4-02bb250f0f22', '77055247078')");
+        $this->addSql("INSERT INTO test_user (uuid, carrier_id, user_identifier) VALUES ('d1dj8f7d-ebd4-12e8-91c4-02bb259d9d8d', '3de06e41-f889-4f95-aac3-ebd31d9d7dg4', '77055247101')");
+        $this->addSql("INSERT INTO test_user (uuid, carrier_id, user_identifier) VALUES ('d1dj8f7d-ebd4-12e8-91c4-02bb2597a6a5', '3de06e41-f889-4f95-aac3-ebd31d9d7dg4', '77055247078')");
     }
 
     public function down(Schema $schema) : void
