b1387e130e0ac44d80e237500fff6b242be369c6
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-12-18 14:38:45 +0200

    EWVS-432: Unique translation fix

--- src/App/Domain/Entity/Translation.php
+++ src/App/Domain/Entity/Translation.php
@@ -8,7 +8,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @UniqueEntity(
- *     fields={"key", "language"},
+ *     fields={"key", "language","carrier"},
  *     message = "Translation with same key already exists"
  * )
  */
