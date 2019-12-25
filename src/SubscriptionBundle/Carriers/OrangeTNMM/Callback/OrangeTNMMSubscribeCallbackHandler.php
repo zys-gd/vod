9ef1acd482472ff782f4c8f0de790f8494331a6e
Yurii Z <RHwqjMZcpvLx5kT8xote9g7tSurlHK1kHySXDSl936Q=>
2019-12-25 17:02:48 +0200

    EWVS-424: * update MM flow

---
+++ src/SubscriptionBundle/Carriers/OrangeTNMM/Callback/OrangeTNMMSubscribeCallbackHandler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace SubscriptionBundle\Carriers\OrangeTNMM\Callback;
+
+use IdentificationBundle\BillingFramework\ID;
+use IdentificationBundle\Entity\User;
+use SubscriptionBundle\BillingFramework\Process\API\DTO\ProcessResult;
+use SubscriptionBundle\Entity\Subscription;
+use SubscriptionBundle\Subscription\Callback\Common\CommonFlowHandler;
+use SubscriptionBundle\Subscription\Callback\Impl\CarrierCallbackHandlerInterface;
+use SubscriptionBundle\Subscription\Callback\Impl\HasCustomConversionTrackingRules;
+use SubscriptionBundle\Subscription\Callback\Impl\HasCustomFlow;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Created by PhpStorm.
+ * User: dmitriy
+ * Date: 01.11.18
+ * Time: 11:18
+ */
+class OrangeTNMMSubscribeCallbackHandler implements CarrierCallbackHandlerInterface, HasCustomConversionTrackingRules, HasCustomFlow
+{
+    /**
+     * @var CommonFlowHandler
+     */
+    private $commonFlowHandler;
+
+    /**
+     * OrangeTNSubscribeCallbackHandler constructor.
+     *
+     * @param CommonFlowHandler $commonFlowHandler
+     */
+    public function __construct(CommonFlowHandler $commonFlowHandler)
+    {
+        $this->commonFlowHandler = $commonFlowHandler;
+    }
+
+    public function canHandle(Request $request, int $carrierId): bool
+    {
+        return $carrierId == ID::ORANGE_TUNISIA_MM;
+    }
+
+    public function afterProcess(Subscription $subscription, User $User, ProcessResult $processResponse)
+    {
+        // TODO: Implement onRenewSendSuccess() method.
+    }
+
+    public function isConversionNeedToBeTracked(ProcessResult $result): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param Request $request
+     * @param string  $type
+     *
+     * @throws \Exception
+     */
+    public function process(Request $request, string $type)
+    {
+        $this->commonFlowHandler->process($request, ID::ORANGE_TUNISIA_MM, $type);
+    }
+}
