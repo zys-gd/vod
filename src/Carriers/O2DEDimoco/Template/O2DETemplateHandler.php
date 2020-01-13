7ce881f23aad8c715bf807be1fc5c767a505ccf5
Denis Lukash <denis.lukash@origin-data.com>
2020-01-13 17:00:10 +0200

    EWVS-452 - template handlers

---
+++ src/Carriers/O2DEDimoco/Template/O2DETemplateHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Carriers\DebitelDEDimoco\Template;
+
+use CommonDataBundle\Service\TemplateConfigurator\Handler\TemplateHandlerInterface;
+use IdentificationBundle\BillingFramework\ID;
+
+/**
+ * Class O2DETemplateHandler
+ */
+class O2DETemplateHandler implements TemplateHandlerInterface
+{
+    /**
+     * @param int $billingCarrierId
+     *
+     * @return bool
+     */
+    public function canHandle(int $billingCarrierId): bool
+    {
+        return $billingCarrierId === ID::O2_DE_DIMOCO;
+    }
+
+    /**
+     * @param string $rootTwigPathAlias
+     * @param string $templatePath
+     * @param string $templateName
+     *
+     * @return string
+     */
+    public function getFullTemplatePath(string $rootTwigPathAlias, string $templatePath, string $templateName): string
+    {
+        return "$rootTwigPathAlias/Impl/O2DEDimoco/$templatePath/$templateName.html.twig";
+    }
+}
